00f4f95a79206bc202db7f558c962cc53d092573
Madamroger <adamsetiadijr07@gmail.com>
2025-02-25 21:24:40 +0700

    remove vercel.json

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({ runtime: 'nodejs@18.x', out: 'standalone', split: false  })
+		adapter: adapter({ runtime: 'nodejs@18.x', out: 'standalone', split: false }),
+		alias: {
+			$lib: 'src/lib',
+		}
 	},
 	extensions: ['.svelte', '.svc']
 };
