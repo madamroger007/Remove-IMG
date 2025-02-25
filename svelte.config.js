055d1eb1196cc7dd8622fba2fa80b35a511a7c0e
Madamroger <adamsetiadijr07@gmail.com>
2025-02-25 21:33:15 +0700

    ganti menjadi adapter vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,10 +11,8 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({ runtime: 'nodejs@18.x', out: 'standalone', split: false }),
-		alias: {
-			$lib: 'src/lib',
-		}
+		adapter: adapter(),
+		
 	},
 	extensions: ['.svelte', '.svc']
 };
